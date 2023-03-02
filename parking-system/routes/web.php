acea14b3702468b288f92068ae613a369192cdf5
nojram00 <113487670+nojram00@users.noreply.github.com>
2023-03-02 19:22:30 +0800

    03-02-23 7:22

--- parking-system/routes/web.php
+++ parking-system/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\mainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/login', function(){
+//     return view('login');
+// });
+
+Route::get('/login', [mainController::class, 'loginPage']);
+
+
