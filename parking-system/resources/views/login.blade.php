e8d0732b7f8ab73dbe9776e6f900c8eb54fd2ddb
nojram00 <113487670+nojram00@users.noreply.github.com>
2023-03-05 15:19:29 +0800

    03-05-23 3:20PM

--- parking-system/resources/views/login.blade.php
+++ parking-system/resources/views/login.blade.php
@@ -7,40 +7,51 @@
     @vite('resources/css/app.css')
     <title>Document</title>
 </head>
-<body class=" bg-slate-400">
+<body>
 
    <!--
         For Login Page:
     -->
 
     {{-- Background --}}
-    <div class="min-h-screen py-40" style="background-image: linear-gradient(115deg, rgb(57, 57, 199), rgba(207, 184, 184, 0.933))">
+    <div class="min-h-screen py-40 bg-no-repeat min-w-full bg-cover" style="background-image: url('images/bg.png')">
         
         {{-- Container --}}
         <div class="container mx-auto">
             
-            <div class="w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden flex">
+            <div class=" flex justify-center">
+                <img src="{{ asset('images/logo2.png') }}" alt="logo" class=" w-60 h-60 mx-20 my-5">
+            </div>
 
-                {{-- Image --}}
-                <div class="w-1/2 bg-slate-500">
-                    <h1>Image Goes here</h1>
-                </div>
-
+            <div class=" w-1/3 rounded-xl mx-auto shadow-lg overflow-hidden flex justify-center" style="background-color: rgba(53, 127, 248, 0.5)">
                 {{-- login --}}
-                <div class="w-1/2 py-16 px-12">
-                    
-                    <h1 class="text-3xl font-bold text-center mb-4">Login</h1>
+                <div class="w-1/2 py-3 px-4">
                     <form action="home" method="GET">
                         <div class="flex flex-col my-2">
                             <input type="text" placeholder="Username" class="border border-blue-900 py-1 px-2 rounded-xl my-1">
                             <input type="password" placeholder="Password" class="border border-blue-900 py-1 px-2 rounded-xl my-1">
                         </div>
                         <div class="flex flex-row justify-center">
-                            <button class=" bg-slate-900 text-white px-7 py-3 rounded-2xl mx-1 my-1">Login</button>
+                            <a href="" class="text-white hover:text-black underline"><h1 class="text-lg font-medium mb-4">Forgot Password?</h1></a>
+                        </div>
+                        <div class="flex flex-row justify-center">
+                            {{-- Login Button --}}
+                            <button class="bg-blue-500 text-white px-7 py-3 rounded-2xl mx-1 my-1 hover:bg-black hover:text-white shadow-black shadow-md">Login</button>
                         </div>
                     </form>
-                    <div class="flex flex-row justify-center">
-                        <a href="register"><button class=" bg-slate-900 text-white px-5 py-3 rounded-2xl mx-1 my-1">Register</button></a>
+                    
+                </div>
+            </div>
+
+            {{-- Go to register --}}
+            <div class="w-1/3 rounded-xl mx-auto shadow-lg overflow-hidden flex justify-center my-10 py-5" style="background-color: rgba(53, 127, 248, 0.5)">
+                <div class="flex flex-col justify-center content-center">
+                    <div>
+                        <h1 class=" text-lg font-medium mb-4 text-white">Don't have an account?</h1>
+                    </div>
+                    <div class="self-center">
+                        {{-- Register Button --}}
+                        <a href="register"><button class="bg-blue-500 text-white px-7 py-3 rounded-2xl mx-1 my-1 hover:bg-black hover:text-white shadow-black shadow-md">Register</button></a>
                     </div>
                 </div>
             </div>
